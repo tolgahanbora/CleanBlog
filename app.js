e2942e25ec30296148cfc0beec2574f525665a3e
tolgahanbora <tolgahanbora@stu.aydin.edu.tr>
2022-10-22 00:53:30 +0300

    added page

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ app.get("/", async (req,res) => {
  })  
 })
 
+app.get("/blogs/:id", async (req,res) => {
+    const blog = await Blogs.findById(req.params.id)
+    res.render("blog", {
+        blog
+    })
+})
+
+
 app.get("/about", (req,res)=> {
     res.render("about")
 })
